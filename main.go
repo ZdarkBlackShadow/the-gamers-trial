0fc7f78d4fe407f16f761ddf90f75e1985de75f4
Adrien LECOMTE <adrien@lecomte.org>
2025-10-24 17:28:32 +0200

    build(app): delete old route, add init env variable, database, template, service, controller and route

--- main.go
+++ main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
-    "log"
+	"html/template"
+	"log"
 
-    "github.com/gofiber/fiber/v2"
+	"github.com/ZdarkBlackShadow/the-gamers-trial/config"
+	"github.com/ZdarkBlackShadow/the-gamers-trial/controller"
+	"github.com/ZdarkBlackShadow/the-gamers-trial/routes"
+	"github.com/ZdarkBlackShadow/the-gamers-trial/service"
+	"github.com/gofiber/fiber/v2"
+	"github.com/subosito/gotenv"
 )
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-    app.Get("/", func (c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
+	err := gotenv.Load()
+	if err != nil {
+		log.Fatal("error when load .env:" + err.Error())
+	}
 
-    log.Fatal(app.Listen(":3000"))
+	db, err := config.InitDatabase()
+	if err != nil {
+		log.Fatal("error when init database:" + err.Error())
+	}
+
+	app.Static("/static", "./assets")
+
+	template, err := template.ParseGlob("./views/*html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	authentificationService := service.InitAuthentificationService(db)
+
+	homeController := controller.InitHomeController(template, authentificationService)
+
+    homeRoutes := app.Group("/home")
+    routes.RegisterDocumentTypesRoutes(homeRoutes, homeController)
+
+	log.Fatal(app.Listen(":8080"))
 }
