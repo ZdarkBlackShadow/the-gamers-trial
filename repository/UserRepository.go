1810a72b2b1598088ec23bebc9f398c72eb8b32c
Adrien LECOMTE <adrien@lecomte.org>
2025-10-25 11:16:44 +0200

    feat(user): update AddUser function for create user in database

--- repository/UserRepository.go
+++ repository/UserRepository.go
@@ -1,11 +1,23 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"github.com/ZdarkBlackShadow/the-gamers-trial/model/entity"
+	"gorm.io/gorm"
+)
 
 type UserRepository struct {
 	db *gorm.DB
 }
 
-func (r *UserRepository) AddUser() {
-	
+func (r *UserRepository) AddUser(user entity.User) (entity.User, error) {
+    if err := r.db.Create(&user).Error; err != nil {
+        return entity.User{}, err
+    }
+
+    var createdUser entity.User
+    if err := r.db.First(&createdUser, user.ID).Error; err != nil {
+        return entity.User{}, err
+    }
+
+    return createdUser, nil
 }
