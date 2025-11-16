081f17dac95daed0a238d489cf91da18f21e0da1
Linijarae <alexis.dupin@ynov.com>
2025-11-16 13:37:02 +0100

    feat(database): add question 2 to seed.go

--- cmd/seed.go
+++ cmd/seed.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"log"
 
-	// Supposons que votre connexion db utilise gorm.DB
-
 	"github.com/ZdarkBlackShadow/the-gamers-trial/model/entity"
 )
 
@@ -15,6 +13,7 @@ func seed() {
 		log.Fatalf("database connection error: %v", err)
 	}
 
+	//Question1 : Minimap Apex Legends
 	imageSearch := entity.Image{Path: "/image.png"}
 	defaultImage := imageSearch
 
@@ -23,9 +22,9 @@ func seed() {
 		log.Fatalf("Erreur seed Image: %v", err)
 	}
 
-	questionSearch := entity.Question{Content: "Dans quel jeu est utilisée cette minimap ?"}
+	questionSearch := entity.Question{ImageID: defaultImage.ID}
 	defaultQuestion := entity.Question{
-		Content: questionSearch.Content,
+		Content: "Dans quel jeu est utilisée cette minimap ?",
 		ImageID: defaultImage.ID,
 	}
 
@@ -48,6 +47,38 @@ func seed() {
 		err := db.FirstOrCreate(&do, searchOption).Error
 		if err != nil {
 			log.Fatalf("Erreur seed Option %s: %v", do.Text, err)
+		}
+	}
+
+	//Question2 : Minimap Animal Crossing
+	image2Search := entity.Image{Path: "/image2.png"}
+	image2 := image2Search
+	err = db.FirstOrCreate(&image2, image2Search).Error
+	if err != nil {
+		log.Fatalf("Erreur seed Image 2: %v", err)
+	}
+
+	question2Search := entity.Question{ImageID: image2.ID}
+	question2 := entity.Question{
+		Content: "Dans quel jeu est utilisée cette minimap ?",
+		ImageID: image2.ID,
+	}
+	err = db.FirstOrCreate(&question2, question2Search).Error
+	if err != nil {
+		log.Fatalf("Erreur seed Question 2: %v", err)
+	}
+
+	options2 := []entity.Option{
+		{Text: "Animal Crossing", QuestionID: question2.ID, Correct: true},
+		{Text: "Pokemon XY", QuestionID: question2.ID, Correct: false},
+		{Text: "Zelda Wind Waker", QuestionID: question2.ID, Correct: false},
+		{Text: "Palworld", QuestionID: question2.ID, Correct: false},
+	}
+	for _, opt := range options2 {
+		searchOpt := entity.Option{Text: opt.Text, QuestionID: question2.ID}
+		err := db.FirstOrCreate(&opt, searchOpt).Error
+		if err != nil {
+			log.Fatalf("Erreur seed Option '%s': %v", opt.Text, err)
 		}
 	}
 
